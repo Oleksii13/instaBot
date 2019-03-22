cfa65f0dc7d979e03b646b2b04fd8047a32932b5
Oleksii <nidjun13@gmail.com>
2019-03-22 18:22:33 -0400

    deploy to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./pages/Main";
-// import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 
